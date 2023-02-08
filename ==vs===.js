651651291c6fe1f92629a29ac985e683a4f108d3
unknown <khankauser916@gmail.com>
2023-02-08 06:52:03 -0800

    ==vs===

---
+++ ==vs===.js
@@ -0,0 +1,36 @@
+// // double equal example
+const num1=5;
+const num2="5";
+if(num1==num2){
+    console.log('true')
+}
+else{
+    console.log("false")
+}
+// === eqal example
+const n1=3;
+const n="3"
+if(n===n1){
+    console.log("this value is true")
+}
+else{
+    console.log("this value is false")
+}
+// another example
+const x=0;
+const y=false;
+if(x==y){
+    console.log("this condition is true ")
+}
+else{
+    console.log("this condition is false")
+}
+// another example
+const a=1;
+const b=true;
+if(a==b){
+    console.log('true')
+}
+else{
+    console.log("false")
+}
