18ff2501bf0316958f10ebf3c2e366b8167761af
unknown <khankauser916@gmail.com>
2023-02-08 10:16:38 -0800

    map-filter-find

---
+++ map-filter.js
@@ -0,0 +1,27 @@
+const numbers=[94,54,32,56,78];
+// const output=[];
+// for(let i=0; i<numbers.length; i++){
+//     const element=numbers[i];
+//     const result=element*element;
+//     output.push(result)
+// }
+// function square(anyNumber){
+//     return anyNumber*anyNumber;
+// }
+// numbers.map(function(anyNumber,anything,check){
+//     console.log(anyNumber,anything,check)
+// })
+// function square(value){
+//     return value*value
+// }
+// const result=numbers.map(function(value){
+//     return value*value;
+// })
+// console.log(result)
+// console.log(output)
+// const result=numbers.map(x=>x*x);
+// console.log(result)
+// const result=numbers.filter(x=>x<78);
+// console.log(result)
+const Find=numbers.find(x=>x<50);
+console.log(Find)
