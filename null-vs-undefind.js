c2ddf122898be9fc3933ebdd20f85173bdb23a88
unknown <khankauser916@gmail.com.com>
2023-02-08 05:15:46 -0800

    true-false

--- null-vs-undefind.js
+++
@@ -1,5 +0,0 @@
-function add(x,y){
-    return x + y;
-}
-const result=add(45,43);
-console.log(result)
---
+++ true-false.js
@@ -0,0 +1,20 @@
+// false value
+// 1.0
+// 2.""
+// 3.undifind
+// 4.NaN;
+// 5.null
+// 6.false
+// true value
+// 1."0"
+// 2." "
+// 3.{}
+// 4.[]
+// 5."false"
+const age=0;
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
