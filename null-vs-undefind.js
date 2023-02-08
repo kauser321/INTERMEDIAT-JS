57fa0ab76277a41eb6e15e2c42be30e5725abcec
unknown <khankauser916@gmail.com>
2023-02-08 06:28:40 -0800

    null-vs-undedind concept

---
+++ null-vs-undefind.js
@@ -0,0 +1,27 @@
+// first waya undefind 
+let pakhi;
+console.log(pakhi);
+// second waya i am geting undefind
+function add(x,y){
+    console.log(x+y);
+}
+const result=add(55,45)
+console.log(result)
+// third way when i am geting undefind
+function number(num1,num2){
+    console.log(num1,num2)
+}
+const res=number(43)
+console.log(res)
+// fourth waya when i geting undefind
+const friend={
+    name:"junayed ahamed sajib",
+    age:22,
+}
+console.log(friend.gfname)
+// fifth waya when i am geting undefind
+const fun=undefined;
+console.log(fun);
+// sixth way when i am geting undefind
+const ages=[34,43,45];
+console.log(ages[34])
